caf14a7642832247cfcdf8aec95ec64223fed57e
NezukoKomada <159033240+NezukoKomada@users.noreply.github.com>
2025-01-23 21:08:37 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function phrases(){
+    window.location = "phrases.html";
+}
+
+function quizzes(){
+    window.location = "quizzes.html";
+}
+
+function home(){
+    window.location = "index.html";
+}
